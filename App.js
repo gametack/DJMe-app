d796412d3331893c28dd58d2ae6ff66ec41985e1
manny <eqo5037@gmail.com>
2019-04-14 18:53:48 -0400

    add aws cognito authentication

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify';
+import awsConfig from './aws-exports';
 
-export default class App extends React.Component {
+Amplify.configure(awsConfig);
+
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
@@ -10,7 +16,7 @@ export default class App extends React.Component {
     );
   }
 }
-
+export default withAuthenticator(App)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
