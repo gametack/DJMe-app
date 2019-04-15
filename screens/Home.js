9fae4223286559df4221c28ee86fb95d552f3b9e
manny <eqo5037@gmail.com>
2019-04-15 01:40:52 -0400

    add home page

---
+++ screens/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class Home extends React.Component {
+    render() {
+      return (
+        <View style={styles.container}>
+            <Button
+            title="Create Party"
+            onPress={() =>
+                this.props.navigation.navigate('CreateParty')
+            }
+            />
+            <Button
+            title="Join Party"
+            onPress={() =>
+                this.props.navigation.navigate('CreateParty')
+            }
+            />            
+        </View>
+      );
+    }
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
